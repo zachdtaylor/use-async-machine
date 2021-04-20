ccb4ca6f244f0df00b17e88e996788cc6e7e7c31
Zach Taylor <zachtylr04@gmail.com>
2021-04-19 23:30:56 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import React from "react";
+import type {
+  UseAsyncEvent,
+  UseAsyncOptions,
+  UseAsyncReducer,
+  UseAsyncState,
+  UseAsync,
+} from "./types";
+
+function transition(state, event, transitions) {
+  return transitions[state.state][event.type]
+    ? transitions[state.state][event.type](event, state)
+    : state;
+}
+
+function exec(state, effects) {
+  return effects[state.state] && effects[state.state](state);
+}
+
+function asyncReducer<TData, TError, TContext>(
+  state: UseAsyncState<TData, TError, TContext>,
+  event: UseAsyncEvent<TData, TError, TContext>
+): UseAsyncState<TData, TError, TContext> {
+  return transition(state, event, {
+    idle: {
+      load: ({ context }) => ({ state: "loading", context }),
+    },
+    loading: {
+      asyncSuccess: ({ data }) => ({ state: "success", data }),
+      asyncError: ({ error }) => ({ state: "error", error }),
+    },
+    success: {
+      load: ({ context }) => ({ state: "loading", context }),
+      reset: () => ({ state: "idle" }),
+    },
+    error: {
+      load: ({ context }) => ({ state: "loading", context }),
+      reset: () => ({ state: "idle" }),
+    },
+  });
+}
+
+const initialState: UseAsyncState = { state: "idle" };
+
+export default function useAsync<TData, TError, TContext>(
+  fn: (state: UseAsyncState<TData, TError, TContext>) => Promise<TData>,
+  options: UseAsyncOptions<TData, TError, TContext> = {}
+): UseAsync<TData, TError, TContext> {
+  const [state, dispatch] = React.useReducer(
+    asyncReducer as UseAsyncReducer<TData, TError, TContext>,
+    initialState as UseAsyncState<TData, TError, TContext>
+  );
+
+  React.useEffect(() => {
+    exec(state, {
+      loading: (state: UseAsyncState<TData, TError, TContext>) => {
+        const promise = fn(state);
+        if (!promise || !promise.then) {
+          throw new Error(
+            `The function passed to useAsync() must return a promise`
+          );
+        }
+        options.onLoad?.(state);
+        promise
+          .then((data) => dispatch({ type: "asyncSuccess", data }))
+          .catch((error) => dispatch({ type: "asyncError", error }));
+      },
+      success: (state: UseAsyncState<TData, TError, TContext>) => {
+        options.onSuccess?.(state);
+      },
+      error: (state: UseAsyncState<TData, TError, TContext>) => {
+        options.onError?.(state);
+      },
+    });
+  }, [state, fn]);
+
+  return { state, dispatch };
+}
---
+++ src/types.ts
@@ -0,0 +1,45 @@
+import type { Dispatch } from "react";
+
+export interface UseAsyncState<
+  TData = unknown,
+  TError = unknown,
+  TContext = unknown
+> {
+  state: "idle" | "loading" | "success" | "error";
+  data?: TData;
+  error?: TError;
+  context?: TContext;
+}
+
+export interface UseAsyncEvent<
+  TData = unknown,
+  TError = unknown,
+  TContext = unknown
+> {
+  type: "load" | "asyncSuccess" | "asyncError" | "reset";
+  data?: TData;
+  error?: TError;
+  context?: TContext;
+}
+
+export interface UseAsyncOptions<
+  TData = unknown,
+  TError = unknown,
+  TContext = unknown
+> {
+  onLoad?: (state: UseAsyncState<TData, TError, TContext>) => unknown;
+  onSuccess?: (state: UseAsyncState<TData, TError, TContext>) => unknown;
+  onError?: (state: UseAsyncState<TData, TError, TContext>) => unknown;
+}
+
+export interface UseAsyncReducer<TData, TError, TContext> {
+  (
+    state: UseAsyncState<TData, TError, TContext>,
+    event: UseAsyncEvent<TData, TError, TContext>
+  ): UseAsyncState<TData, TError, TContext>;
+}
+
+export interface UseAsync<TData, TError, TContext> {
+  state: UseAsyncState<TData, TError, TContext>;
+  dispatch: Dispatch<UseAsyncEvent<TData, TError, TContext>>;
+}
